2227d5568aa9fee01e636a0a7354a59a4baa3307
CelsoTNeto <cteixeiraneto@hotmail.com>
2018-08-30 20:26:12 -0300

    Aula do dia 30 de agosto

---
+++ Funcao_multiplica.js
@@ -0,0 +1,10 @@
+function multiplicacao (a,b){
+  
+		 var resultado = a*b;
+		 
+		 console.log ("imprimindo resultado " + resultado);
+		 return a*b;
+		 }
+		 
+		 var ResultadoDaFuncao=multiplicacao (5,2);
+		 alert (ResultadoDaFuncao);
