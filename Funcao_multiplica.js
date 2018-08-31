efa3f8a1c0fc33fb05f033c9c57f383086990281
CelsoTNeto <cteixeiraneto@hotmail.com>
2018-08-30 22:16:35 -0300

    Introduzindo um loop

--- Funcao_multiplica.js
+++ Funcao_multiplica.js
@@ -7,4 +7,16 @@ function multiplicacao (a,b){
 		 }
 		 
 		 var ResultadoDaFuncao=multiplicacao (5,2);
-		 alert (ResultadoDaFuncao);
+		// alert (ResultadoDaFuncao);
+
+function mostraCarros (recebeCarros){
+	var i;
+	for ( i=0; i <recebeCarros.length;i++){
+		console.log (recebeCarros[i]);
+	}
+}
+
+var Carros = ["Mustang", "ford", "camaro", "companheiro", "Vo ve e te aviso"]
+
+mostraCarros (Carros);{
+console.log ("Quantidade de objetos na função carros: " + Carros.length);}
